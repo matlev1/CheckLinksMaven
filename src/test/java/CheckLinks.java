d2cc82e0f048d303ea5e4b8ec5beede9b945a98a
Levon Matevosyan <levon@Levon-Matevosyans-MacBookPro.local>
2017-07-27 23:25:25 -0700

    committing code with redirects

--- src/test/java/CheckLinks.java
+++ src/test/java/CheckLinks.java
@@ -7,12 +7,11 @@ import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.safari.SafariDriver;
-
 import java.io.File;
+import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class CheckLinks
 {
@@ -22,12 +21,12 @@ public class CheckLinks
 
         List<WebElement> elementList = new ArrayList();
 
-        elementList = driver.findElements(By.tagName("a"));
+            elementList = driver.findElements(By.tagName("a"));
 
         elementList.addAll(driver.findElements(By.tagName("img")));
 
         List finalList = new ArrayList();
-        ;
+
 
         for (WebElement element : elementList)
 
@@ -46,39 +45,57 @@ public class CheckLinks
         return finalList;
 
     }
+    static String getLocation;
 
-    public static Integer isLinkBroken(URL url) throws Exception
+    public static Integer getResponse(String url)
 
     {
 
         //url = new URL("http://yahoo.com");
-        int responseStatus;
+        int responseStatus = 0;
+
 
         String response = "";
-
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-        try
-
-        {
-
-            connection.connect();
-
-            responseStatus = connection.getResponseCode();
-            //response = connection.getResponseMessage() + " " + responseStatus;
+        HttpURLConnection con = null;
+        try {
 
 
-            connection.disconnect();
+            con = (HttpURLConnection) new URL(url).openConnection();
+            con.setInstanceFollowRedirects(true);
+            con.setRequestMethod("GET");
+            con.connect();
+            responseStatus = con.getResponseCode();
+            getLocation = con.getHeaderField("Location");
 
-            return responseStatus;
-
-        } catch (Exception exp)
-
-        {
-
-            return 0;
-
+            //System.out.println(con.getResponseMessage() + " " + responseStatus);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    finally {
+    con.disconnect();
+}
+
+        //HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+
+
+
+           ;
+
+            //responseStatus = connection.getResponseCode();
+            //response =
+        //
+
+
+
+
+
+
+
+
+
+        return responseStatus;
+
 
     }
 
@@ -91,45 +108,84 @@ public class CheckLinks
         //driver = new FirefoxDriver();
         System.setProperty("webdriver.chrome.driver", "/Users/levon/ChromeDriver/chromedriver");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://timeandexpense.teksystems.com/webtime/");
+        //driver.get("https://timeandexpense.teksystems.com/webtime/");
 
-        driver.get("https://www.apple.com/mac/");
+        driver.get("https://www.apple.com/today/");
 
         //ff.get("http://www.yahoo.com/");
 
-        List<WebElement> allImages = findAllLinks(driver);
+        List<WebElement> allLinks = findAllLinks(driver);
 
-        System.out.println("Total number of elements found " + allImages.size());
+        System.out.println("Total number of elements found " + allLinks.size());
 
-        for (WebElement element : allImages)
+
+        for (WebElement element : allLinks)
         {
 
             try
 
             {
+                int responseStatus;
                 //String responseString = isLinkBroken(new URL(element.getAttribute("href"));
                 JavascriptExecutor js = (JavascriptExecutor) driver;
-                if (isLinkBroken(new URL(element.getAttribute("href"))) == 200)
+                if (getResponse(element.getAttribute("href")) == 200)
                 {
                     js.executeScript("arguments[0].setAttribute('style', arguments[1]);", element, "background-color: lightgreen; border: 1px dotted solid lightgreen;");
                 }
-                else if (isLinkBroken(new URL(element.getAttribute("href"))) == 404)
+                else if (getResponse(element.getAttribute("href")) == 404)
                 {
                     js.executeScript("arguments[0].setAttribute('style', arguments[1]);", element, "background-color: red; border: 1px dotted solid red;");
+                }
+                else if ((getResponse(element.getAttribute("href")) == 301) )
+                {
+                    String desiredText = Integer.toString(getResponse(element.getAttribute("href")));
+                    js.executeScript("arguments[0].setAttribute('style', arguments[1]);", element, "background-color: lightgrey; border: 1px dotted solid lightblue;");
+                    js.executeScript("arguments[0].innerHTML='" + desiredText + "'", element);
+                    System.out.println("This is where 301s are being redirected" + " " + getLocation);
+
+
+                    HttpURLConnection con = (HttpURLConnection) new URL(getLocation).openConnection();
+                    con.setInstanceFollowRedirects(false);
+                    con.setRequestMethod("GET");
+                    con.connect();
+                    responseStatus = con.getResponseCode();
+                    String newLocation = con.getHeaderField("Location");
+                    con.disconnect();
+
+                    System.out.println("Second Level Redirect Location and response status " + responseStatus + " " + newLocation );
+
+                    if (responseStatus != 200 ) {
+                       HttpURLConnection conec = (HttpURLConnection) new URL(newLocation).openConnection();
+                       conec.setInstanceFollowRedirects(false);
+                       // con.setRequestMethod("GET");
+                       // con.connect();
+                        //responseStatus = conec.getResponseCode();
+                        String newNewlocation = conec.getHeaderField("Location");
+                        //con.disconnect();
+                        if (!newNewlocation.contains("http://www.apple.com"))
+                        {
+                            System.out.println("Third Level Redirect Location and response status " + responseStatus + " http://www.apple.com" + newNewlocation );
+                        }
+                        else
+                        System.out.println("Third Level Redirect Location and response status " + responseStatus + " " + newNewlocation );
+                    }
+
+
                 }
 
                 else
                 {
-                    String desiredText = Integer.toString(isLinkBroken(new URL(element.getAttribute("href"))));
+                    String desiredText = Integer.toString(getResponse(element.getAttribute("href")));
                     js.executeScript("arguments[0].setAttribute('style', arguments[1]);", element, "background-color: lightblue; border: 1px dotted solid lightblue;");
                     //element.findElement(By)
                     //js.executeScript("document.getElementsByTagName('')).innerHTML"+ desiredText);
                     //js.executeScript("document.evaluate(html/body/footer/div/nav[2]/div[2]/div/ul/li, document, null, 9, null).singleNodeValue.innerHTML"+ desiredText);
                     //("arguments[0].innerHTML = '<h1>Set text using innerHTML</h1>'", element)
                     js.executeScript("arguments[0].innerHTML='" + desiredText + "'", element);
+                    System.out.println("URL: " + element.getAttribute("href") + " returned " + getResponse(element.getAttribute("href")));
                 }
 
-                System.out.println("URL: " + element.getAttribute("href") + " returned " + isLinkBroken(new URL(element.getAttribute("href"))));
+
 
 
                 //System.out.println("URL: " + element.getAttribute("outerhtml")+ " returned " + isLinkBroken(new URL(element.getAttribute("href"))));
